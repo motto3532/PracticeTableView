1ea49ea06a390cb36608874adf3f18287f79569d
motto3532 <gropmor74@gmail.com>
2023-06-13 23:01:05 +0900

    Initial Commit

---
+++ PracticeTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeTableView
+//
+//  Created by Atto Rari on 2023/06/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
